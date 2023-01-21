54293a31a566949669049199e2a1d3a6dac5b857
Gugdhh <122460282+Gugdhh@users.noreply.github.com>
2023-01-22 01:07:38 +0530

    Update Bikash.py

--- Bikash.py
+++ Bikash.py
@@ -9,12 +9,12 @@ import re
 
 API_ID = os.environ.get("API_ID", "none") 
 API_HASH = os.environ.get("API_HASH", "none") 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "none") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5955905234:AAHCDzOTHfi235ViELPneYmXhf52W05wKjQ) 
 MONGO_URL = os.environ.get("MONGO_URL", "none")
 BOT_IMAGE = os.environ.get("BOT_IMAGE", "none")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "none")
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "none")
-SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "none")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "HOTX_CHATING_BOT")
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ITS_SILENCER_BABU")
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "X_TEAM_IMMORTAL_X")
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "none")
 
 
